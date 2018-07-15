2a83f85e7515c57525f664dcc2acfc7088a1d795
Iain Buclaw <ibuclaw@gdcproject.org>
2018-07-16 00:33:33 +0200

    Commit first working configuration

---
+++ agents/install-target-deps.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# This file switches on the shell variable ${target}, and also uses the
+# following shell variables:
+#
+#  install_packages     List of all common packages to install.
+#
+#  binutils_packages    List of all binutils-related packages to install.
+#
+#  libc_packages        List of all libc-related packages to install.
+
+target=${1}
+install_packages="gcc-${DOCKER_GCC_VERSION} g++-${DOCKER_GCC_VERSION} gdc-${DOCKER_GCC_VERSION} \
+    autogen autoconf2.64 automake1.11 bison dejagnu flex make patch"
+binutils_packages="binutils"
+libc_packages="libc6-dev"
+
+# Install base dependencies.
+apt-get update -qq
+apt-get install --no-install-recommends -qq \
+    apt-transport-https bzip2 curl software-properties-common xz-utils
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update -qq
+
+# Do we need something more specific for the worker?
+case ${target} in
+    ubuntu-cross-all)
+        binutils_packages="${binutils_packages} \
+            binutils-aarch64-linux-gnu \
+            binutils-alpha-linux-gnu \
+            binutils-arm-linux-gnueabi \
+            binutils-arm-linux-gnueabihf \
+            binutils-arm-none-eabi \
+            binutils-hppa-linux-gnu \
+            binutils-hppa64-linux-gnu \
+            binutils-mips-linux-gnu \
+            binutils-mips64-linux-gnuabi64 \
+            binutils-mips64el-linux-gnuabi64 \
+            binutils-mipsel-linux-gnu \
+            binutils-powerpc-linux-gnu \
+            binutils-powerpc-linux-gnuspe \
+            binutils-powerpc64-linux-gnu \
+            binutils-powerpc64le-linux-gnu \
+            binutils-s390x-linux-gnu \
+            binutils-sh4-linux-gnu \
+            binutils-sparc64-linux-gnu"
+        libc_packages="${libc_packages} \
+            libc6.1-dev-alpha-cross \
+            libc6-dev-arm64-cross \
+            libc6-dev-armel-armhf-cross \
+            libc6-dev-armel-cross \
+            libc6-dev-armhf-cross \
+            libc6-dev-armhf-armel-cross \
+            libc6-dev-armhf-cross \
+            libc6-dev-hppa-cross \
+            libc6-dev-mips-cross \
+            libc6-dev-mips32-mips64-cross \
+            libc6-dev-mips32-mips64el-cross \
+            libc6-dev-mips64-cross \
+            libc6-dev-mips64-mips-cross \
+            libc6-dev-mips64-mipsel-cross \
+            libc6-dev-mips64el-cross \
+            libc6-dev-mipsel-cross \
+            libc6-dev-mipsn32-mips-cross \
+            libc6-dev-mipsn32-mips64-cross \
+            libc6-dev-mipsn32-mips64el-cross \
+            libc6-dev-mipsn32-mipsel-cross \
+            libc6-dev-powerpc-cross \
+            libc6-dev-powerpc-ppc64-cross \
+            libc6-dev-powerpcspe-cross \
+            libc6-dev-ppc64-cross \
+            libc6-dev-ppc64-powerpc-cross \
+            libc6-dev-ppc64el-cross \
+            libc6-dev-s390-s390x-cross \
+            libc6-dev-s390x-cross \
+            libc6-dev-sh4-cross \
+            libc6-dev-sparc64-cross \
+            libc6-dev-sparc-sparc64-cross"
+        ;;
+
+    *)
+        ;;
+esac
+
+apt-get install --no-install-recommends -qq \
+    $install_packages $binutils_packages $libc_packages
+
+# Install buildkite agent.
+echo "deb https://apt.buildkite.com/buildkite-agent stable main" > /etc/apt/sources.list.d/buildkite-agent.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 32A37959C2FA5C3C99EFBC32A79206696452D198
+apt-get update -qq
+apt-get install -qq buildkite-agent
+
+# Clean-up.
+apt-get clean
+rm -rf /var/lib/apt/lists/*
---
+++ agents/pipeline.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# List of all target name/triplet that we build.
+targets=(
+    #'ubuntu-alpha/alpha-linux-gnu'
+    'ubuntu-arm/arm-linux-gnueabi'
+    'ubuntu-armhf/arm-linux-gnueabihf'
+    'ubuntu-aarch64/aarch64-linux-gnu'
+    #'ubuntu-hppa/hppa-linux-gnu'
+    #'ubuntu-hppa64/hppa64-linux-gnu'
+    'ubuntu-mips/mips-linux-gnu'
+    #'ubuntu-mips64/mips64-linux-gnuabi64'
+    'ubuntu-mips64el/mips64el-linux-gnuabi64'
+    'ubuntu-mipsel/mipsel-linux-gnu'
+    #'ubuntu-powerpc/powerpc-linux-gnu'
+    #'ubuntu-powerpc-spe/powerpc-linux-gnuspe'
+    #'ubuntu-powerpc64/powerpc64-linux-gnu'
+    'ubuntu-powerpc64le/powerpc64le-linux-gnu'
+    'ubuntu-systemz/s390x-linux-gnu'
+    #'ubuntu-sh4/sh4-linux-gnu'
+    'ubuntu-sparc64/sparc64-linux-gnu'
+)
+
+# Agents where host == target.
+declare -A native_targets
+native_targets['ubuntu-arm']=1
+native_targets['ubuntu-armhf']=1
+native_targets['ubuntu-aarch64']=1
+
+cat << 'EOF'
+steps:
+EOF
+
+for target in "${targets[@]}"; do
+    name=$(cut -d/ -f1 <<< ${target})
+    triplet=$(cut -d/ -f2 <<< ${target})
+
+    # Don't build the self-hosted compiler on native platforms.
+    # Unfortunately we don't have a working D compiler for them.
+    if [ "${native_targets[$name]:-x}" != 'x' ]; then
+        if [ `expr "${BUILDKITE_BRANCH}" : 'stable'` -ne 0 ]; then
+            host='native'
+        else
+            host='cross'
+        fi
+    else
+        host='cross'
+    fi
+
+cat << EOF
+  - label: "${name}"
+    command: |
+      echo "--- Configure gdc"
+      /buildkite/buildci.sh configure
+      echo "--- Build gdc"
+      /buildkite/buildci.sh build
+      echo "--- Run testsuite"
+      /buildkite/buildci.sh testsuite
+      echo "--- Run unittests"
+      /buildkite/buildci.sh unittests
+    env:
+      BUILDKITE_TARGET: ${triplet}
+      BUILDKITE_CACHE_DIR: '/buildkite/cache'
+    agents:
+      - ${name}=1
+      - ${host}=1
+
+EOF
+done
