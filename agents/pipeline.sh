8ac1f5d7fe8e70ec7cd08a6cac67c8874e05a6ca
Iain Buclaw <ibuclaw@gdcproject.org>
2021-05-03 16:57:05 +0200

    agents/pipeline.sh: Simplify bootstrap/native logic, and add openbsd

--- agents/pipeline.sh
+++ agents/pipeline.sh
@@ -2,32 +2,37 @@
 
 # List of all target name/triplet that we build.
 targets=(
-    #'ubuntu-alpha/alpha-linux-gnu'
-    'ubuntu-arm/arm-linux-gnueabi'
-    'ubuntu-armhf/arm-linux-gnueabihf'
-    'ubuntu-aarch64/aarch64-linux-gnu'
-    #'ubuntu-hppa/hppa-linux-gnu'
-    #'ubuntu-hppa64/hppa64-linux-gnu'
-    'ubuntu-mips/mips-linux-gnu'
-    #'ubuntu-mips64/mips64-linux-gnuabi64'
-    'ubuntu-mips64el/mips64el-linux-gnuabi64'
-    'ubuntu-mipsel/mipsel-linux-gnu'
-    #'ubuntu-powerpc/powerpc-linux-gnu'
-    #'ubuntu-powerpc64/powerpc64-linux-gnu'
-    'ubuntu-powerpc64le/powerpc64le-linux-gnu'
-    'ubuntu-systemz/s390x-linux-gnu'
-    #'ubuntu-sh4/sh4-linux-gnu'
-    'ubuntu-sparc64/sparc64-linux-gnu'
-    'ubuntu-x86_64/x86_64-linux-gnu'
+    #'ubuntu-alpha/alpha-linux-gnu/linux'
+    'ubuntu-arm/arm-linux-gnueabi/linux'
+    'ubuntu-armhf/arm-linux-gnueabihf/linux'
+    'ubuntu-aarch64/aarch64-linux-gnu/linux'
+    #'ubuntu-hppa/hppa-linux-gnu/linux'
+    #'ubuntu-hppa64/hppa64-linux-gnu/linux'
+    'ubuntu-mips/mips-linux-gnu/linux'
+    #'ubuntu-mips64/mips64-linux-gnuabi64/linux'
+    'ubuntu-mips64el/mips64el-linux-gnuabi64/linux'
+    'ubuntu-mipsel/mipsel-linux-gnu/linux'
+    #'ubuntu-powerpc/powerpc-linux-gnu/linux'
+    #'ubuntu-powerpc64/powerpc64-linux-gnu/linux'
+    'ubuntu-powerpc64le/powerpc64le-linux-gnu/linux'
+    'ubuntu-systemz/s390x-linux-gnu/linux'
+    #'ubuntu-sh4/sh4-linux-gnu/linux'
+    'ubuntu-sparc64/sparc64-linux-gnu/linux'
+    'ubuntu-x86_64/x86_64-linux-gnu/linux'
+    'openbsd-amd64/amd64-unknown-openbsd6.9/openbsd'
 )
 
 # Agents where host == target.
 declare -A native_targets
 native_targets['ubuntu-x86_64']=1
-declare -A native_ports
-#native_ports['ubuntu-arm']=1
-#native_ports['ubuntu-armhf']=1
-#native_ports['ubuntu-aarch64']=1
+#native_targets['ubuntu-arm']=1
+#native_targets['ubuntu-armhf']=1
+#native_targets['ubuntu-aarch64']=1
+native_targets['openbsd-amd64']=1
+
+# Agents where to build bootstrap.
+declare -A bootstrap_targets
+bootstrap_targets['ubuntu-x86_64']=1
 
 cat << 'EOF'
 steps:
@@ -36,28 +41,20 @@ EOF
 for target in "${targets[@]}"; do
     name=$(cut -d/ -f1 <<< ${target})
     triplet=$(cut -d/ -f2 <<< ${target})
+    os=$(cut -d/ -f3 <<< ${target})
     bootstrap='disable'
 
-    # Don't build the self-hosted compiler on native ports.
+    # TODO: Get a working D compiler on native targets.
     # Unfortunately we don't have a working D compiler for them.
-    if [ "${native_ports[$name]:-x}" != 'x' ]; then
-        if [ `expr "${BUILDKITE_BRANCH}" : '.*master-ci'` -ne 0 ]; then
-            host='native'
-        elif [ `expr "${BUILDKITE_PULL_REQUEST_BASE_BRANCH}" : '.*master-ci'` -ne 0 ]; then
-            host='native'
-        else
-            host='cross'
-        fi
-    elif [ "${native_targets[$name]:-x}" != 'x' ]; then
-        # Test bootstrap builds on master branches where supported.
-        if [ `expr "${BUILDKITE_BRANCH}" : '.*master-ci'` -eq 0 ]; then
-            bootstrap='enable'
-        elif [ `expr "${BUILDKITE_PULL_REQUEST_BASE_BRANCH}" : '.*master-ci'` -eq 0 ]; then
+    if [ "${native_targets[$name]:-x}" != 'x' ]; then
+        host='native'
+        if [ "${bootstrap_targets[$name]:-x}" != 'x' ]; then
+            host='bootstrap'
             bootstrap='enable'
         fi
-        host='bootstrap'
     else
         host='cross'
+        os='linux'
     fi
 
 cat << EOF
@@ -72,6 +69,7 @@ cat << EOF
       echo "--- Run unittests"
       ./buildci.sh unittests
     env:
+      BUILDKITE_OS: ${os}
       BUILDKITE_TARGET: ${triplet}
       BUILDKITE_CACHE_DIR: '/buildkite/cache'
       BUILDKITE_BOOTSTRAP: ${bootstrap}
